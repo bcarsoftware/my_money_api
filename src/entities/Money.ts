af0b86057ed18d1f1917e5ddae1abeef1c718bd8
Abel Carvalho <abelbcarvalho.dev@gmail.com>
2026-08-21 23:06:51 -0300

    feat: define money relationship to user

--- src/entities/Money.ts
+++ src/entities/Money.ts
@@ -3,9 +3,12 @@ import {
   CreateDateColumn,
   DeleteDateColumn,
   Entity,
+  JoinColumn,
+  ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from "typeorm";
+import { User } from "./User";
 
 @Entity("money")
 export class Money {
@@ -14,8 +17,21 @@ export class Money {
   @Column({ length: 64 })
   tag: string;
 
+  @Column({ type: "decimal", precision: 10, scale: 2, nullable: true })
+  objective?: number;
+
+  @Column({ length: 256, nullable: true })
+  description?: string;
+
   @Column({ type: "decimal", precision: 10, scale: 2 })
   balance: number;
+
+  @Column({ type: "uuid", name: "user_id" })
+  userId: string;
+
+  @ManyToOne(() => User, (user) => user.id)
+  @JoinColumn({ name: "user_id", referencedColumnName: "id" })
+  user: User;
 
   @CreateDateColumn({ name: "created_at" })
   createdAt: Date;
