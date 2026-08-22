5eeb5a3ef3eb97c140efe5000292149e4617bd4a
Abel Carvalho <abelbcarvalho.dev@gmail.com>
2026-08-22 12:58:08 -0300

    refactor: prettier applied

--- src/migration/1787412644065-CreateBank.ts
+++ src/migration/1787412644065-CreateBank.ts
@@ -1,20 +1,29 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class CreateBank1787412644065 implements MigrationInterface {
-    name = 'CreateBank1787412644065'
+  name = "CreateBank1787412644065";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TYPE "public"."banks_account_type_enum" AS ENUM('SAVING', 'CHECKING', 'INVESTMENT', 'PAYMENT')`);
-        await queryRunner.query(`CREATE TABLE "banks" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "user_id" uuid NOT NULL, "code" character varying(8) NOT NULL, "name" character varying(64) NOT NULL, "account_type" "public"."banks_account_type_enum" NOT NULL, "accountNumber" character varying(64) NOT NULL, "agency" character varying(32) NOT NULL, "balance" numeric(10,2) NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP, CONSTRAINT "PK_3975b5f684ec241e3901db62d77" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE UNIQUE INDEX "IDX_banks_user_id_code" ON "banks"  ("user_id", "code") `);
-        await queryRunner.query(`ALTER TABLE "banks" ADD CONSTRAINT "FK_bank_user_id" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TYPE "public"."banks_account_type_enum" AS ENUM('SAVING', 'CHECKING', 'INVESTMENT', 'PAYMENT')`
+    );
+    await queryRunner.query(
+      `CREATE TABLE "banks" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "user_id" uuid NOT NULL, "code" character varying(8) NOT NULL, "name" character varying(64) NOT NULL, "account_type" "public"."banks_account_type_enum" NOT NULL, "accountNumber" character varying(64) NOT NULL, "agency" character varying(32) NOT NULL, "balance" numeric(10,2) NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP, CONSTRAINT "PK_3975b5f684ec241e3901db62d77" PRIMARY KEY ("id"))`
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_banks_user_id_code" ON "banks"  ("user_id", "code") `
+    );
+    await queryRunner.query(
+      `ALTER TABLE "banks" ADD CONSTRAINT "FK_bank_user_id" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "banks" DROP CONSTRAINT "FK_bank_user_id"`);
-        await queryRunner.query(`DROP INDEX "public"."IDX_banks_user_id_code"`);
-        await queryRunner.query(`DROP TABLE "banks"`);
-        await queryRunner.query(`DROP TYPE "public"."banks_account_type_enum"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "banks" DROP CONSTRAINT "FK_bank_user_id"`
+    );
+    await queryRunner.query(`DROP INDEX "public"."IDX_banks_user_id_code"`);
+    await queryRunner.query(`DROP TABLE "banks"`);
+    await queryRunner.query(`DROP TYPE "public"."banks_account_type_enum"`);
+  }
 }
