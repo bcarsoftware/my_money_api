ad7c454b520d6065dfff8b4f709e3e972fcc1f30
Abel Carvalho <abelbcarvalho.dev@gmail.com>
2026-08-22 18:54:12 -0300

    feat: use context to logged user

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -4,4 +4,5 @@ export const __prod__ = process.env.NODE_ENV === "production";
 export const __prodLike__ = __prod__ || process.env.NODE_ENV === "staging";
 
 export const TOKEN_INVALID = "token invalid";
+
 export const USER_NOT_FOUND = "user not found";
