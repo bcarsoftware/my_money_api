081db12977b2387b5bf34b780de46951d2a044db
Abel Carvalho <abelbcarvalho.dev@gmail.com>
2026-08-22 12:56:36 -0300

    feat: invoice entity created

---
+++ src/entities/Invoice.ts
@@ -0,0 +1,59 @@
+import { RepeatEnum } from "@/enums/RepeatEnum";
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { Bank } from "./Bank";
+import { InvoiceStatusEnum } from "@/enums/InvoiceStatusEnum";
+
+@Entity("invoices")
+export class Invoice extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid") id: string;
+
+  @Column({ type: "uuid", name: "bank_id" })
+  bankId: string;
+
+  @ManyToOne(() => Bank, (bank) => bank.id)
+  @JoinColumn({ name: "bank_id", referencedColumnName: "id" })
+  bank: Bank;
+
+  @Column({ type: "varchar", length: 64 })
+  name: string;
+
+  @Column({ type: "varchar", length: 256, nullable: true })
+  description?: string;
+
+  @Column({ type: "enum", enum: RepeatEnum })
+  repeat: RepeatEnum;
+
+  @Column({ type: "int", default: 1 })
+  installments: number;
+
+  @Column({ type: "int", default: 0, name: "paid_installments" })
+  paidInstallments: number;
+
+  @Column({ type: "decimal", precision: 10, scale: 2 })
+  balance: number;
+
+  @Column({ type: "decimal", precision: 10, scale: 2 })
+  total: number;
+
+  @Column({ type: "enum", enum: InvoiceStatusEnum })
+  status: InvoiceStatusEnum;
+
+  @CreateDateColumn({ name: "created_at" })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: "updated_at" })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ name: "deleted_at" })
+  deletedAt: Date;
+}
