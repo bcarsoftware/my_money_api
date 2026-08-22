d71a9ffdee0fed3ddd5b0e533bf5b9edb4bd151d
Abel Carvalho <abelbcarvalho.dev@gmail.com>
2026-08-22 12:06:43 -0300

    feat: data source load migration

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,7 +1,10 @@
 import "dotenv/config";
+import path from "path";
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from "./entities/User";
+
+const migrationsDir = path.join(__dirname, "migrations");
 
 export const AppDataSource = new DataSource({
   type: "postgres",
@@ -13,6 +16,12 @@ export const AppDataSource = new DataSource({
   synchronize: true,
   logging: false,
   entities: [User],
-  migrations: [],
+  migrations: [`${migrationsDir}/*.{ts,js}`],
+  migrationsTransactionMode: "each",
+  extra: {
+    max: 10,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 5000,
+  },
   subscribers: [],
 });
