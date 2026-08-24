ea32bab84836a36c67a1ef3a1054d7e2a08badb1
Abel Carvalho <abelbcarvalho.dev@gmail.com>
2026-08-23 22:07:14 -0300

    feat: is username decorator and test

---
+++ src/utils/verifiers/decorators/IsUsername.ts
@@ -0,0 +1,33 @@
+import { usernameVerify } from "@/utils/verifiers/usernameVerify";
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+} from "class-validator";
+
+export const IS_USERNAME = "isUsername";
+
+export function isUsername(value: unknown): boolean {
+  return typeof value === "string" && usernameVerify(value);
+}
+
+export function IsUsername(
+  validationOptions?: ValidationOptions
+): PropertyDecorator {
+  return function (object: object, propertyName: string | symbol) {
+    registerDecorator({
+      name: IS_USERNAME,
+      target: object.constructor,
+      propertyName: propertyName as string,
+      options: validationOptions,
+      validator: {
+        validate(value: unknown, _args: ValidationArguments) {
+          return isUsername(value);
+        },
+        defaultMessage(args: ValidationArguments) {
+          return `${args.property} must be a valid username. It should start with a lowercase letter, can contain lowercase letters, numbers, underscores, and hyphens, and must be between 3 and 128 characters long.`;
+        },
+      },
+    });
+  };
+}
