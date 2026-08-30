21df646ebcb5e1e7f383e00d5740c63e6fa3f4ee
Abel Carvalho <abelbcarvalho.dev@gmail.com>
2026-08-30 14:06:46 -0300

    feat: dto and util

---
+++ src/resolvers/bank/dto/toBankDto.ts
@@ -0,0 +1,13 @@
+import { Bank } from "@/entities/Bank";
+
+export const toBankDto = (bank: Bank) => ({
+  id: bank.id,
+  userId: bank.userId,
+  code: bank.code,
+  name: bank.name,
+  accountType: bank.accountType,
+  accountNumber: bank.accountNumber,
+  agency: bank.agency,
+  balance: bank.balance,
+  createdAt: bank.createdAt,
+});
