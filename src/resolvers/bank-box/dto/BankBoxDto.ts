9e67839fbec7b6937a4a07e3b9e7de0748b93ef2
Abel Carvalho <abelbcarvalho.dev@gmail.com>
2026-08-30 21:57:13 -0300

    feat: bank box dto

---
+++ src/resolvers/bank-box/dto/BankBoxDto.ts
@@ -0,0 +1,34 @@
+import { Field, ObjectType } from "type-graphql";
+
+@ObjectType()
+export class BankBoxDto {
+  @Field(() => String)
+  id: string;
+
+  @Field(() => String)
+  bankId: string;
+
+  @Field(() => String)
+  tag: string;
+
+  @Field(() => String, { nullable: true })
+  objective?: string | null;
+
+  @Field(() => String, { nullable: true })
+  description?: string | null;
+
+  @Field(() => String)
+  balance: string;
+
+  @Field(() => Date)
+  createdAt: Date;
+}
+
+@ObjectType()
+export class PaginatedBankBoxDto {
+  @Field(() => [BankBoxDto])
+  items: BankBoxDto[];
+
+  @Field(() => Number)
+  total: number;
+}
