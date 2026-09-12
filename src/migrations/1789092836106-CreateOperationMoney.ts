bc03c30d9bb89a58dacdff650afaeb5f0e16d73b
Abel Carvalho <abelbcarvalho.dev@gmail.com>
2026-09-12 16:38:36 -0300

    migrate: uuid to varchar 64

--- src/migrations/1789092836106-CreateOperationMoney.ts
+++ src/migrations/1789092836106-CreateOperationMoney.ts
@@ -13,7 +13,7 @@ export class CreateOperationMoney1789092836106 implements MigrationInterface {
       `CREATE TYPE "public"."operations_money_local_enum" AS ENUM('INTERNAL', 'EXTERNAL')`
     );
     await queryRunner.query(
-      `CREATE TABLE "operations_money" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "operation_register" uuid NOT NULL, "user_id" uuid NOT NULL, "money_id" uuid NOT NULL, "tag" character varying(64) NOT NULL, "description" character varying(256), "balance" numeric(10,2) NOT NULL, "discount" numeric(10,2), "forfeit" numeric(10,2), "amount" numeric(10,2) NOT NULL, "type_operation" "public"."operations_money_type_operation_enum" NOT NULL, "local" "public"."operations_money_local_enum" NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_operations_money" PRIMARY KEY ("id"))`
+      `CREATE TABLE "operations_money" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "operation_register" character varying(64) NOT NULL, "user_id" uuid NOT NULL, "money_id" uuid NOT NULL, "tag" character varying(64) NOT NULL, "description" character varying(256), "balance" numeric(10,2) NOT NULL, "discount" numeric(10,2), "forfeit" numeric(10,2), "amount" numeric(10,2) NOT NULL, "type_operation" "public"."operations_money_type_operation_enum" NOT NULL, "local" "public"."operations_money_local_enum" NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_operations_money" PRIMARY KEY ("id"))`
     );
     await queryRunner.query(
       `ALTER TABLE "operations_money" ADD CONSTRAINT "FK_operations_money_user" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
